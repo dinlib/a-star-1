25e54acd6c669d8d2b82727de02022664fcbbbd2
pauloapanucci <pauloapanucci@gmail.com>
2016-12-29 19:12:21 -0200

    na traaaaave

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC = g++ -std=c++11 main.cpp -o a.out -Wall
 
 runcodes: compile_runcodes rcodes
 
-report: compile_normal tests
+report: compile_runcodes tests
 
 compile_normal:
 	g++ -std=c++11 main.cpp -o a.out -Wall
@@ -12,35 +12,45 @@ compile_runcodes:
 
 tests:
 	@echo "MOA REPORT - 15 PUZZLE A* TESTS"
-
-	@echo "CASE 1"
+	@echo
+	@echo "CASE 1 ------------------------- EXPECTED 21"
+	@echo -n "answer = "
 	@time ./a.out  2 3 4 8 5 1 6 7 10 11 12 15 0 9 13 14
-
-	@echo "CASE 2"
+	@echo
+	@echo "CASE 2 ------------------------- EXPECTED 53"
+	@echo -n "answer = "
 	@time ./a.out 8 7 6 5 2 3 1 15 4 12 13 0 10 11 9 14
-
-	@echo "CASE 3"
+	@echo
+	@echo "CASE 3 ------------------------- EXPECTED 53"
+	@echo -n "answer = "
 	@time ./a.out 7 0 6 5 8 2 3 1 12 13 9 15 4 10 14 11
-
-	@echo "CASE 4"
+	@echo
+	@echo "CASE 4 ------------------------- EXPECTED 53"
+	@echo -n "answer = "
 	@time ./a.out 3 9 5 1 6 2 13 11 7 8 12 14 0 4 10 15
-
-	@echo "CASE 5"
+	@echo
+	@echo "CASE 5 ------------------------- EXPECTED 50"
+	@echo -n "answer = "
 	@time ./a.out 9 2 1 11 3 8 5 14 6 4 13 15 7 10 12 0
-
-	@echo "CASE 6"
+	@echo
+	@echo "CASE 6 ------------------------- EXPECTED ?"
+	@echo -n "answer = "
 	@time ./a.out 0 2 1 11 9 3 8 5 7 6 4 14 10 12 15 13
-
-	@echo "CASE 7"
+	@echo
+	@echo "CASE 7 ------------------------- EXPECTED 50"
+	@echo -n "answer = "
 	@time ./a.out 2 3 11 5 9 6 1 14 7 12 8 13 10 15 4 0
-
-	@echo "CASE 8"
+	@echo
+	@echo "CASE 8 ------------------------- EXPECTED ?"
+	@echo -n "answer = "
 	@time ./a.out 3 11 5 0 2 6 1 14 9 12 8 13 7 10 15 4
-
-	@echo "CASE 9"
+	@echo
+	@echo "CASE 9 ------------------------- EXPECTED 53"
+	@echo -n "answer = "
 	@time ./a.out 3 11 5 14 0 2 6 1 12 8 4 13 9 7 10 15
-
-	@echo "CASE 10"
+	@echo
+	@echo "CASE 10 ------------------------- EXPECTED ?"
+	@echo -n "answer = "
 	@time ./a.out 11 5 14 1 3 2 6 13 0 8 4 15 12 9 7 10
 
 rcodes:
@@ -54,18 +64,18 @@ rcodes:
 	@echo -n "answer = "
 	@time ./a.out 5 1 2 3 6 7 11 4 13 9 10 8 14 15 12 0
 	@echo
-	#@echo "CASE 3 ------------------------- EXPECTED 23"
-	#@echo -n "answer = "
-	#@time ./a.out 6 5 1 2 0 7 11 3 13 9 10 4 14 15 12 8
+	@echo "CASE 3 ------------------------- EXPECTED 23"
+	@echo -n "answer = "
+	@time ./a.out 6 5 1 2 0 7 11 3 13 9 10 4 14 15 12 8
 	@echo
-	@echo "CASE 4 ------------------------- EXPECTED 33"
+	@echo "CASE 4 ------------------------- EXPECTED 31"
 	@echo -n "answer = "
 	@time ./a.out 6 5 1 2 13 7 11 3 9 10 4 8 0 14 15 12
 	@echo
-	@echo "CASE 5 ------------------------- EXPECTED ?"
+	@echo "CASE 5 ------------------------- EXPECTED 44"
 	@echo -n "answer = "
 	@time ./a.out 9 5 11 3 13 15 7 0 1 10 2 4 6 12 8 14
 	@echo
-	@echo "CASE 6 ------------------------- EXPECTED ?"
+	@echo "CASE 6 ------------------------- EXPECTED 41"
 	@echo -n "answer = "
 	@time ./a.out 9 5 7 11 1 13 15 3 10 0 2 4 6 12 8 14
--- run_codes_submission.cpp
+++ run_codes_submission.cpp
@@ -1,35 +1,38 @@
 #include <iostream>
 #include <sstream>
 #include <queue>
-// #include <tr1/unordered_map>
-#include <unordered_map>
+#include <tr1/unordered_map>
+// #include <unordered_map>
 #include <string.h>
 #include <set>
 #include <map>
 #include <stdlib.h>
 
 int solution[4][4] =  {
-                        {1, 2, 3, 4},
-                        {5, 6, 7, 8},
-                        {9, 10, 11, 12},
-                        {13, 14, 15, 0}
-                      };
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12},
+    {13, 14, 15, 0}
+};
 
-std::pair<int,int> correct_board_place[16] = { std::make_pair(3, 3), std::make_pair(0, 0), std::make_pair(0, 1), std::make_pair(0, 2),
-                                               std::make_pair(0, 3), std::make_pair(1, 0), std::make_pair(1, 1), std::make_pair(1, 2),
-                                               std::make_pair(1, 3), std::make_pair(2, 0), std::make_pair(2, 1), std::make_pair(2, 2),
-                                               std::make_pair(2, 3), std::make_pair(3, 0), std::make_pair(3, 1), std::make_pair(3, 2)
-                                            };
+std::string strsolution = "01020304050607080910111213141500";
 
-std::pair<int,int> board_places[16] = { std::make_pair(0, 0), std::make_pair(0, 1), std::make_pair(0, 2), std::make_pair(0, 3),
-                                        std::make_pair(1, 0), std::make_pair(1, 1), std::make_pair(1, 2), std::make_pair(1, 3),
-                                        std::make_pair(2, 0), std::make_pair(2, 1), std::make_pair(2, 2), std::make_pair(2, 3),
-                                        std::make_pair(3, 0), std::make_pair(3, 1), std::make_pair(3, 2), std::make_pair(3, 3)
-                                      };
+std::pair<int,int> correct_board_place[16] = {
+    std::make_pair(3, 3), std::make_pair(0, 0), std::make_pair(0, 1), std::make_pair(0, 2),
+    std::make_pair(0, 3), std::make_pair(1, 0), std::make_pair(1, 1), std::make_pair(1, 2),
+    std::make_pair(1, 3), std::make_pair(2, 0), std::make_pair(2, 1), std::make_pair(2, 2),
+    std::make_pair(2, 3), std::make_pair(3, 0), std::make_pair(3, 1), std::make_pair(3, 2)
+};
 
+std::pair<int,int> board_places[16] = {
+    std::make_pair(0, 0), std::make_pair(0, 1), std::make_pair(0, 2), std::make_pair(0, 3),
+    std::make_pair(1, 0), std::make_pair(1, 1), std::make_pair(1, 2), std::make_pair(1, 3),
+    std::make_pair(2, 0), std::make_pair(2, 1), std::make_pair(2, 2), std::make_pair(2, 3),
+    std::make_pair(3, 0), std::make_pair(3, 1), std::make_pair(3, 2), std::make_pair(3, 3)
+};
 
 typedef struct state {
-    state *parent;
+    // state *parent;
     std::string hash_key;
     int f;
     int steps;
@@ -39,7 +42,7 @@ typedef struct state {
 
 struct state_cmp {
     bool operator() (const state& lhs, const state& rhs) const {
-        if (lhs.f == rhs.f) return lhs.heuristic < rhs.heuristic;
+        if (lhs.f == rhs.f) return rhs.heuristic < lhs.heuristic;
         else return lhs.f < rhs.f;
     }
 };
@@ -55,8 +58,8 @@ std::string generate_hash_key(int board [][4]){
     return hash_key.str();
 }
 
-// typedef std::tr1::unordered_map<std::string, state> hash_table;
-typedef std::unordered_map<std::string, state> hash_table;
+typedef std::tr1::unordered_map<std::string, state> hash_table;
+// typedef std::unordered_map<std::string, state> hash_table;
 typedef std::multiset<state, state_cmp> mset;
 typedef mset::iterator set_it;
 typedef hash_table::iterator hash_it;
@@ -84,7 +87,6 @@ typedef struct OpenCustom{
         std::string backup_key = it->hash_key;
         open_set.erase(it);
         open_hash.erase(backup_key);
-
         return st;
     }
 
@@ -101,24 +103,30 @@ typedef struct OpenCustom{
     }
 
     bool empty(){
-        return open_hash.empty();
+        return open_set.empty();
+    }
+
+    int size(){
+        return open_set.size();
     }
 
 } OpenCustom;
 
-bool check_solution(int matrix[][4]){
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            if(matrix[i][j] != solution[i][j]){
-                return false;
-            }
-        }
-    }
-    return true;
+//CHECK 15-PUZZLE SOLUTION
+bool check_solution(std::string board){
+    // for (int i = 0; i < 4; i++) {
+    //     for (int j = 0; j < 4; j++) {
+    //         if(matrix[i][j] != solution[i][j]){
+    //             return false;
+    //         }
+    //     }
+    // }
+    // return true;
+    if(board.compare(strsolution) == 0) return true;
+    else return false;
 }
 
-//****HEURISTICS****
-
+//HEURISTICS
 int heuristic_1(int board[][4]){ //HEURISTIC 1
     int heuristic = 0;
     for(int i = 0; i < 4; i++) {
@@ -131,14 +139,14 @@ int heuristic_1(int board[][4]){ //HEURISTIC 1
     return heuristic;
 }
 
-int heuristic(int board[][4]){ //HEURISTIC 2
+int heuristic_2(int board[][4]){ //HEURISTIC 2
     int heuristic = 0;
     for(int i = 1; i < 16; i++) {
         std::pair<int, int> last = board_places[i - 1];
         std::pair<int, int> current = board_places[i];
         if(board[current.first][current.second] != board[last.first][last.second] + 1)
-          if(board[last.first][last.second] != 0)
-              heuristic++;
+        if(board[last.first][last.second] != 0)
+        heuristic++;
     }
     if(board[3][3] == 0) heuristic--;
     return heuristic;
@@ -146,31 +154,34 @@ int heuristic(int board[][4]){ //HEURISTIC 2
 
 int manhattan_distance(int index, int r, int c){
     std::pair<int, int> correct_pos = correct_board_place[index];
-    return (abs(correct_pos.first - r) + abs(correct_pos.second - c));
+    int manhattan_distance = (abs(correct_pos.first - r) + abs(correct_pos.second - c));
+    return manhattan_distance;
 }
 
-int heuristic_3(int board[][4]){ //HEURISTIC 3
+int heuristic(int board[][4]){ //HEURISTIC 3
     int heuristic = 0;
     for(int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            if(board[i][j] != solution[i][j]){
-              heuristic += manhattan_distance(board[i][j], i, j);
+            if((board[i][j] != solution[i][j]) ){
+                if(board[i][j] != 0){
+                    heuristic += manhattan_distance(board[i][j], i, j);
+                }
             }
         }
     }
     return heuristic;
 }
 
-// int heuristic_4(int board[][4]){ //HEURISTIC 4
+// int heuristic(int board[][4]){ //HEURISTIC 4
 //     int h1 = heuristic_1(board);
 //     int h2 = heuristic_2(board);
 //     int h3 = heuristic_3(board);
 //     int p1 = 0;
 //     int p2 = 0;
-//     int p3 = 0;
+//     int p3 = 1;
 //     return p1*h1 + p2*h2 + p3*h3;
 // }
-//
+
 // int heuristic_5(int board[][4]){ //HEURISTIC 5
 //     int h1 = heuristic_1(board);
 //     int h2 = heuristic_2(board);
@@ -179,12 +190,13 @@ int heuristic_3(int board[][4]){ //HEURISTIC 3
 // }
 
 
-void copy_board(int origin[4][4], int destiny[4][4]){
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            destiny[i][j] = origin[i][j];
-        }
-    }
+void copy_board(int origin[][4], int destiny[][4]){
+    // for (int i = 0; i < 4; i++) {
+    //     for (int j = 0; j < 4; j++) {
+    //         destiny[i][j] = origin[i][j];
+    //     }
+    // }
+     memcpy(destiny, origin, 4*4*sizeof(int));
 }
 
 
@@ -192,96 +204,99 @@ std::queue<state> generate_sucessors(state st){
     std::queue<state> sucessors;
     for(int i = 0; i < 4; i++){
         for (int j = 0; j < 4; j++) {
-           if(st.board[i][j] == 0){
-               if(i - 1 >= 0){ //trocar com a peça de cima
-                   state up;
-                   up.parent = &st;
-                   up.steps = st.steps + 1;
-                   copy_board(st.board, up.board);
+            if(st.board[i][j] == 0){
+                if(i != 0){ //trocar com a peça de cima
+                    state up;
+                    // up.parent = &st;
+                    up.steps = st.steps + 1;
+                    copy_board(st.board, up.board);
 
-                   int aux = up.board[i][j];
-                   up.board[i][j] = up.board[i - 1][j];
-                   up.board[i - 1][j] = aux;
+                    int aux = up.board[i][j];
+                    up.board[i][j] = up.board[i - 1][j];
+                    up.board[i - 1][j] = aux;
 
-                   up.heuristic = heuristic(up.board);
-                   up.f = up.steps + up.heuristic;
-                   up.hash_key = generate_hash_key(up.board);
-                   sucessors.push(up);
-               }
-               if(i + 1 <= 4 - 1){ //trocar com a peça de  baixo
-                   state down;
-                   down.parent = &st;
+                    // up.heuristic = heuristic(up.board);
+                    // up.f = up.steps + up.heuristic;
+                    up.hash_key = generate_hash_key(up.board);
+                    sucessors.push(up);
+                }
+                if(i != 3){ //trocar com a peça de  baixo
+                    state down;
+                    // down.parent = &st;
 
-                   down.steps = st.steps + 1;
-                   copy_board(st.board, down.board);
+                    down.steps = st.steps + 1;
+                    copy_board(st.board, down.board);
 
-                   int aux = down.board[i][j];
-                   down.board[i][j] = down.board[i + 1][j];
-                   down.board[i + 1][j] = aux;
+                    int aux = down.board[i][j];
+                    down.board[i][j] = down.board[i + 1][j];
+                    down.board[i + 1][j] = aux;
 
-                   down.heuristic = heuristic(down.board);
-                   down.f = down.steps + down.heuristic;
-                   down.hash_key = generate_hash_key(down.board);
-                   sucessors.push(down);
-               }
-               if(j - 1 >= 0){ //trocar com a peça da esquerda
-                   state left;
-                   left.parent = &st;
-                   left.steps = st.steps + 1;
-                   copy_board(st.board, left.board);
+                    // down.heuristic = heuristic(down.board);
+                    // down.f = down.steps + down.heuristic;
+                    down.hash_key = generate_hash_key(down.board);
+                    sucessors.push(down);
+                }
+                if(j != 0){ //trocar com a peça da esquerda
+                    state left;
+                    // left.parent = &st;
+                    left.steps = st.steps + 1;
+                    copy_board(st.board, left.board);
 
-                   int aux = left.board[i][j];
-                   left.board[i][j] = left.board[i][j - 1];
-                   left.board[i][j - 1] = aux;
+                    int aux = left.board[i][j];
+                    left.board[i][j] = left.board[i][j - 1];
+                    left.board[i][j - 1] = aux;
 
-                   left.heuristic = heuristic(left.board);
-                   left.f = left.steps + left.heuristic;
-                   left.hash_key = generate_hash_key(left.board);
-                   sucessors.push(left);
-               }
-               if(j + 1 <= 4 - 1){ //trocar com a peça da direita
-                   state right;
-                   right.parent = &st;
-                   right.steps = st.steps + 1;
-                   copy_board(st.board, right.board);
+                    // left.heuristic = heuristic(left.board);
+                    // left.f = left.steps + left.heuristic;
+                    left.hash_key = generate_hash_key(left.board);
+                    sucessors.push(left);
+                }
+                if(j != 3){ //trocar com a peça da direita
+                    state right;
+                    // right.parent = &st;
+                    right.steps = st.steps + 1;
+                    copy_board(st.board, right.board);
 
-                   int aux = right.board[i][j];
-                   right.board[i][j] = right.board[i][j + 1];
-                   right.board[i][j + 1] = aux;
+                    int aux = right.board[i][j];
+                    right.board[i][j] = right.board[i][j + 1];
+                    right.board[i][j + 1] = aux;
 
-                   right.heuristic = heuristic(right.board);
-                   right.f = right.steps + right.heuristic;
-                   right.hash_key = generate_hash_key(right.board);
-                   sucessors.push(right);
-               }
-           }
+                    // right.heuristic = heuristic(right.board);
+                    // right.f = right.steps + right.heuristic;
+                    right.hash_key = generate_hash_key(right.board);
+                    sucessors.push(right);
+                }
+            }
         }
     }
     return sucessors;
 }
 
 int a_star(int board[][4]){
+
+    //BEGIN STATE
     state begin_state;
-    begin_state.parent = NULL;
+    // begin_state.parent = NULL;
     begin_state.steps = 0;
-    for(int i = 0; i < 4; i++){
-        for (int j = 0; j < 4; j++) {
-            begin_state.board[i][j] = board[i][j];
-        }
-    }
+    copy_board(board, begin_state.board);
     begin_state.heuristic = heuristic(begin_state.board);
     begin_state.f = begin_state.heuristic + begin_state.steps;
     begin_state.hash_key = generate_hash_key(begin_state.board);
 
+    //OPEN AND CLOSED SET
     OpenCustom open;
     hash_table closed;
 
+    // int phase = 0;
+
+    //A-STAR ALGORITHM BEGIN
     open.insert(&begin_state);
     while(!open.empty()){
+
         state st = open.extract_min();
         closed[st.hash_key] = st;
 
-        if(check_solution(st.board)){
+        if(check_solution(st.hash_key)){
             return st.steps;
         }
 
@@ -291,21 +306,60 @@ int a_star(int board[][4]){
             state suc = sucessors.front();
             hash_it open_it = open.find(suc.hash_key);
             hash_it closed_it = closed.find(suc.hash_key);
-            if(open_it != open.end()){
-                if(suc.steps < open_it->second.steps){
-                    open.remove(open_it);
+            bool isInOpen = (open_it != open.end());
+            bool isInClosed = (closed_it != closed.end());
+
+            if(isInClosed){
+                if(suc.steps < closed_it->second.steps){
+                    closed.erase(closed_it);
+                    suc.heuristic = heuristic(suc.board);
+                    suc.f = suc.steps + suc.heuristic;
                     open.insert(&suc);
                 }
             }
-            else if(closed_it != closed.end()){
-                if(suc.steps < closed_it->second.steps){
-                    closed.erase(closed_it->first);
+            else if(isInOpen){
+                if(suc.steps < open_it->second.steps){
+                    open.remove(open_it);
+                    suc.heuristic = heuristic(suc.board);
+                    suc.f = suc.steps + suc.heuristic;
                     open.insert(&suc);
                 }
             }
             else{
+                suc.heuristic = heuristic(suc.board);
+                suc.f = suc.steps + suc.heuristic;
                 open.insert(&suc);
             }
+
+            // if(isInOpen && (suc.steps < open_it->second.steps)){
+            //     open.remove(open_it);
+            // }
+            // if(isInClosed && (suc.steps < closed_it->second.steps)){
+            //     closed.erase(closed_it);
+            // }
+            // if(!(isInOpen || isInClosed)){
+            //     suc.heuristic = heuristic(suc.board);
+            //     suc.f = suc.steps + suc.heuristic;
+            //     open.insert(&suc);
+            // }
+
+            // if(!isInClosed){
+            //     if(isInOpen){
+            //         if(suc.steps < open_it->second.steps){
+            //             open.remove(open_it);
+            //             suc.heuristic = heuristic(suc.board);
+            //             suc.f = suc.steps + suc.heuristic;
+            //             open.insert(&suc);
+            //         }
+            //     }
+            //     else {
+            //         suc.heuristic = heuristic(suc.board);
+            //         suc.f = suc.steps + suc.heuristic;
+            //         open.insert(&suc);
+            //     }
+            // }
+
+
             sucessors.pop();
         }
     }
@@ -314,27 +368,13 @@ int a_star(int board[][4]){
 
 
 int main(int argc, char *argv[]) {
-
-    // std::pair<int, int> test;
-    // for (size_t i = 0; i < 16; i++) {
-    //   test = correct_board_place[i];
-    //   std::cout << "Tile[" << i << "] = " << test.first << " " << test.second << std::endl;
-    // }
-
-    // for (size_t i = 0; i < 4; i++) {
-    //   for (size_t j = 0; j < 4; j++) {
-    //     std::cout << solution[i][j] << std::endl;
-    //   }
-    // }
-
-
     int initial_board[4][4];
     int count = 1;
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-          initial_board[i][j] = atoi(argv[count]);
-          count++;
-          // std::cin >> initial_board[i][j];
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            initial_board[i][j] = atoi(argv[count]);
+            count++;
+            // std::cin >> initial_board[i][j];
         }
     }
     int solution = a_star(initial_board);
